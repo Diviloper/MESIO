6d3c360a223b4ea3dfb7f1a9ba0c63de98810df3
victor.divi <victor.divi@upc.edu>
2025-02-19 20:02:21 +0100

    Add initial (incomplete) version of primal-affine scaling

---
+++ ogd/primal_affine_scaling.jl
@@ -0,0 +1,43 @@
+using LinearAlgebra
+
+function primal_affine_scaling(A::Matrix{Float64}, b::Vector{Float64}, c::Vector{Float64}, ϵ::Float64, ρ::Float64)::Vector{Float64}
+
+    xᵏ = initial()
+    while true
+        Δᵏ, y = direction(A, c, xᵏ);
+        α = step_size(xᵏ, Δᵏ, ρ);
+        xᵏ = xᵏ + α*Δᵏ;
+
+        stop(b, c, xᵏ, y, ϵ) || break;
+    end
+
+    return xᵏ;
+end
+
+function initial()::Vector{Float64}
+    # TODO: Complete initial
+end
+
+function stop(b::Vector{Float64}, c::Vector{Float64}, x::Vector{Float64}, y::Vector{Float64}, ϵ::Float64)::Bool
+    cᵀx = tr(c)*x;
+    bᵀy = tr(b)*y;
+    
+    return abs(cᵀx - bᵀy) / (1 + abs(cᵀx)) <= ϵ;
+end
+
+function direction(A::Matrix{Float64}, c::Vector{Float64}, xᵏ::Vector{Float64})::Tuple{Vector{Float64}, Vector{Float64}}
+    Aᵀ = tr(A);
+
+    D = Diagonal(xᵏ)^2;
+    y =  inv(A*D*Aᵀ)*A*D*c;
+    z = c - Aᵀ*y;
+
+    Δᵏ = -D*z;
+
+    return Δᵏ, y
+end
+
+function step_size(xᵏ::Vector{Float64}, Δᵏ::Vector{Float64}, ρ::Float64)::Vector{Float64}
+    ᾱ = min(-xᵏᵢ/Δᵏᵢ for (xᵏᵢ, Δᵏᵢ) = zip(xᵏ,Δᵏ) if Δᵏᵢ<= 0);
+    return ρ*ᾱ;
+end
