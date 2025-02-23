ba2ffae24c6a1abb5c5f3776e98d88a304bd48cf
Diviloper <vdiviloper@gmail.com>
2025-02-23 19:39:40 +0100

    Finish (functional) basic implementation. Add structs to represent Problem and Result

--- ogd/primal_affine_scaling.jl
+++ ogd/primal_affine_scaling.jl
@@ -1,43 +1,96 @@
 using LinearAlgebra, LinearSolve
 
-function primal_affine_scaling(A::Matrix{Float64}, b::Vector{Float64}, c::Vector{Float64}, ϵ::Float64, ρ::Float64)::Vector{Float64}
+const VF = Vector{Float64}
+const VVF = Vector{VF}
+const MF = Matrix{Float64}
 
-    xᵏ = initial()
-    while true
-        Δᵏ, y = direction(A, c, xᵏ);
-        α = step_size(xᵏ, Δᵏ, ρ);
-        xᵏ = xᵏ + α*Δᵏ;
+struct Problem
+    A::MF
+    b::VF
+    c::VF
+end
 
-        stop(b, c, xᵏ, y, ϵ) || break;
+struct Result
+    o::VF
+    x::VVF
+    Δ::VVF
+    gap::VF
+end
+
+function primal_affine_scaling(P::Problem, ϵ::Float64=10e-8, ρ::Float64=0.995)::Result
+
+    PM, x̄⁰ = expand_problem(P)
+
+    (; o, x, Δ, gap) = primal_affine_scaling(PM, x̄⁰, ϵ, ρ)
+
+    if o[end] > ϵ
+        throw("Problem is unfeasible: xₙ₊₁ = $(x[end])")
     end
 
-    return xᵏ;
+    return Result(o[1:end-1], [xᵢ[1:end-1] for xᵢ in x], [Δᵢ[1:end-1] for Δᵢ in Δ], gap)
 end
 
-function initial()::Vector{Float64}
-    # TODO: Complete initial
+function primal_affine_scaling(P::Problem, x⁰::VF, ϵ::Float64, ρ::Float64)::Result
+    # Constants
+    (; A, b, c) = P
+    Aᵀ = transpose(A)
+
+    # History
+    x::VVF = [x⁰]
+    Δ::VVF = []
+    gap::VF = []
+
+    # Initializations
+    k = 1
+    D = Diagonal(x[k])^2
+    y = compute(A * D * Aᵀ, A * D * c)
+    push!(gap, dual_gap(b, c, x[k], y))
+
+    while gap[k] > ϵ
+        z = c - Aᵀ * y
+        push!(Δ, -D * z) # Δₖ = -Dz
+
+        if all(Δ[k] .>= 0)
+            throw("Unbounded Problem")
+        end
+
+        α = ρ * minimum(-xᵏᵢ / Δᵏᵢ for (xᵏᵢ, Δᵏᵢ) = zip(x[k], Δ[k]) if Δᵏᵢ <= 0)
+        push!(x, x[k] + α * Δ[k]) # xᵏ = αΔᵏ
+
+        k += 1
+
+        D = Diagonal(x[k])^2
+        y = compute(A * D * Aᵀ, A * D * c)
+        push!(gap, dual_gap(b, c, x[k], y))
+    end
+
+    return Result(round.(x[k], digits=4), x, Δ, gap);
 end
 
-function stop(b::Vector{Float64}, c::Vector{Float64}, x::Vector{Float64}, y::Vector{Float64}, ϵ::Float64)::Bool
-    cᵀx = transpose(c)*x;
-    bᵀy = transpose(b)*y;
-    
-    return abs(cᵀx - bᵀy) / (1 + abs(cᵀx)) <= ϵ;
+function expand_problem(P::Problem)::Tuple{Problem,VF}
+    (; A, b, c) = P
+
+    n = size(c, 1)
+    x⁰ = [1.0 for _ = 1:n]
+
+    r = b - A * x⁰
+    Ā = hcat(A, r)
+
+    M = sum(abs.(c)) * 10000
+    c̄ = push!(copy(c), M)
+
+    x̄⁰ = push!(x⁰, 1)
+
+    return Problem(Ā, b, c̄), x̄⁰
 end
 
-function direction(A::Matrix{Float64}, c::Vector{Float64}, xᵏ::Vector{Float64})::Tuple{Vector{Float64}, Vector{Float64}}
-    Aᵀ = transpose(A);
+function dual_gap(b::VF, c::VF, x::VF, y::VF)::Float64
+    cᵀx = transpose(c) * x
+    bᵀy = transpose(b) * y
 
-    D = Diagonal(xᵏ)^2;
-    y =  solve(A*D*Aᵀ, A*D*c);
-    z = c - Aᵀ*y;
-
-    Δᵏ = -D*z;
-
-    return Δᵏ, y
+    return abs(cᵀx - bᵀy) / (1 + abs(cᵀx))
 end
 
-function step_size(xᵏ::Vector{Float64}, Δᵏ::Vector{Float64}, ρ::Float64)::Vector{Float64}
-    ᾱ = min(-xᵏᵢ/Δᵏᵢ for (xᵏᵢ, Δᵏᵢ) = zip(xᵏ,Δᵏ) if Δᵏᵢ<= 0);
-    return ρ*ᾱ;
+function compute(A::MF, b::VF)::VF
+    return solve(LinearProblem(A, b)).u
 end
