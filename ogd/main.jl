823f949cc5375d08a034942d76cfb627cbeeaf65
victor.divi <victor.divi@upc.edu>
2025-03-11 16:52:17 +0100

    Add tolerances to main

--- ogd/main.jl
+++ ogd/main.jl
@@ -4,12 +4,12 @@ include("primal_affine_scaling.jl")
 include("jump.jl")
 include("runner.jl")
 
-function run_all_in_dir(dir_path)
+function run_all_in_dir(dir_path, ϵ::Float64=1e-5, ρ::Float64=0.6)
     problems = readdir(dir_path, join=true)
 
     for problem in problems
         @info "Running $(basename(problem))"
-        p, r, tr = run_from_mat_file(problem)
+        p, r, tr = run_from_mat_file(problem, ϵ, ρ)
         @info "Finished $(basename(problem)): PAS: $(p.c'r.o) | Tulip: $(p.c'tr)"
     end
 end
