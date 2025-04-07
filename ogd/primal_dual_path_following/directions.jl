9a173dc89d3a6925757c313eab3ab49a66dcc688
Diviloper <vdiviloper@gmail.com>
2025-04-07 19:35:20 +0200

    Add augmented and normal systems for newton step. Fix pdpf algorithm

--- ogd/primal_dual_path_following/directions.jl
+++ ogd/primal_dual_path_following/directions.jl
@@ -1,20 +1,39 @@
 abstract type Step end
 
+@enum System begin
+    base
+    augmented
+    normal
+end
+
 struct NewtonStep <: Step
     σ::Float64
+    system::System
 end
+NewtonStep(σ::Float64) = NewtonStep(σ, normal)
+NewtonStep() = NewtonStep(0.1, normal)
 
 struct MehrotraStep <: Step
 end
 
-function compute_direction(step::NewtonStep, A::SMF, x::VF, λ::VF, s::VF)::Tuple{VF, VF, VF, Float64}
+function compute_direction(step::NewtonStep, P::StandardProblem, x::VF, λ::VF, s::VF)::Tuple{VF,VF,VF,Float64}
+    if step.system == base
+        return newton_base_system(step, P, x, λ, s)
+    elseif step.system == augmented
+        return newton_augmented_system(step, P, x, λ, s)
+    elseif step.system == normal
+        return newton_normal_system(step, P, x, λ, s)
+    end
+end
+
+function newton_base_system(step::NewtonStep, (; A, b, c)::StandardProblem, x::VF, λ::VF, s::VF)::Tuple{VF,VF,VF,Float64}
     # Vars
     (m, n) = size(A)
-    (;σ) = step
+    (; σ) = step
 
-    X = spdiagm(x) 
+    X = spdiagm(x)
     S = spdiagm(s)
-    
+
     e = ones(n)
     I = spdiagm(e)
 
@@ -23,24 +42,94 @@ function compute_direction(step::NewtonStep, A::SMF, x::VF, λ::VF, s::VF)::Tupl
     Zⁿᵐ = sparse([n], [m], [0])
     Zⁿ = sparse([n], [n], [0])
 
-    μ = x's/n
+    μ = x's / n
 
     # System construction
 
     F = dropzeros!([
-        Zⁿ A'  I  ;
-        A  Zᵐ  Zᵐⁿ;
-        S  Zⁿᵐ X
+        Zⁿ  A'  I  ;
+        A   Zᵐ  Zᵐⁿ;
+        S   Zⁿᵐ X
     ])
 
     rᶜ = A' * λ + s - c
     rᵇ = A * x - b
-    rˣˢ= X*S*e - σ*μ*e
+    rˣˢ = X * S * e - σ * μ * e
     R = [-rᶜ; -rᵇ; -rˣˢ]
 
-    r = solve(LinearProblem(F, R))
-    
-    return r[1:n], r[n+1:n+m], r[n+m+1:end], μ
+    Δ = solve(LinearProblem(F, R)).u
+
+    return Δ[1:n], Δ[n+1:n+m], Δ[n+m+1:end], μ
+end
+
+function newton_augmented_system(step::NewtonStep, (; A, b, c)::StandardProblem, x::VF, λ::VF, s::VF)::Tuple{VF,VF,VF,Float64}
+    # Vars
+    (m, n) = size(A)
+    (; σ) = step
+
+    X = spdiagm(x)
+    X⁻¹ = spdiagm(1 ./ x)
+    S = spdiagm(s)
+
+    Θ = -spdiagm(s./x) # -(X*S⁻¹)⁻¹,  Makes matrix below prettier :)
+
+    e = ones(n)
+
+    Zᵐ = sparse([m], [m], [0])
+
+    μ = x's / n
+
+    # System construction
+
+    F = dropzeros!([
+        Θ  A';
+        A  Zᵐ ;
+    ])
+
+    rᶜ = A' * λ + s - c
+    rᵇ = A * x - b
+    rˣˢ = X * S * e - σ * μ * e
+    R = [-rᶜ + X⁻¹*rˣˢ; -rᵇ]
+
+    r = solve(LinearProblem(F, R)).u
+
+    Δx = r[1:n]
+    Δλ = r[n+1:n+m]
+    Δs = -X⁻¹*(rˣˢ + S*Δx)
+
+    return Δx, Δλ, Δs, μ
+end
+
+function newton_normal_system(step::NewtonStep, (; A, b, c)::StandardProblem, x::VF, λ::VF, s::VF)::Tuple{VF,VF,VF,Float64}
+    # Vars
+    (m, n) = size(A)
+    (; σ) = step
+
+    X = spdiagm(x)
+    X⁻¹ = spdiagm(1 ./ x)
+    S = spdiagm(s)
+    S⁻¹ = spdiagm(1 ./ s)
+
+    Θ = X*S⁻¹
+
+    e = ones(n)
+
+    μ = x's / n
+
+    # System construction
+
+    AΘAᵀ = A * Θ * A'
+
+    rᶜ = A' * λ + s - c
+    rᵇ = A * x - b
+    rˣˢ = X * S * e - σ * μ * e
+
+    R = -rᵇ + A * Θ * (-rᶜ + X⁻¹ * rˣˢ)
+    Δλ = solve(LinearProblem(AΘAᵀ, R)).u
+    Δs = -rᶜ - A'Δλ
+    Δx = -S⁻¹ * (rˣˢ + X*Δs)
+
+    return Δx, Δλ, Δs, μ
 end
 
 function compute_direction(step::MehrotraStep, A::SMF, x::SMF, s::SMF)
